c94ea4aba2532bc0df436a08f44d8c2bee5ffe27
kylemasc917 <kylemasc@buffalo.edu>
2024-10-12 20:57:50 +0000

    Added necessary files for completing the assignment and structure for future edits.

---
+++ ResistanceInAWire/Resistance_In_A_Wire_Algorithm_Using_Lambda_Function.py
@@ -0,0 +1,5 @@
+""" This contains an algorithm module that find the resistance in a wire using the lambda function. """
+
+#Define a lamba function to calculate the resistance of a wire
+calculate_RW = lambda rho, l, A: (rho * l) / A
+
---
+++ ResistanceInAWire/Resistance_In_A_Wire_Algorithm_Using_Reduce_Function.py
@@ -0,0 +1,4 @@
+""" This contains an algorithm module that find the resistance in a wire using the reduce function. """
+
+import functools import reduce
+"""importing functional programming tools and reduce function specifically """
