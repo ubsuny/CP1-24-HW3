1f3c0696ef28a43f7cb30cc1fdfb701939599aec
ojha-aditya <adityaojha.science@gmail.com>
2024-10-14 10:58:17 -0400

    Update TestPoyntingVector.py
    Completed the unit test and added docstrings

--- PoyntingVectorModule/TestPoyntingVector.py
+++ PoyntingVectorModule/TestPoyntingVector.py
@@ -1,23 +1,34 @@
-import PoyntingVector
+'''
+This module runs the unittest for the functions in PoyntingVector.py module 
+'''
 import numpy as np
 import sympy as sp
 import math
-import pytest
+import unittest
 
-def Test_PoyntingVector():
-    E_0 = 5
-    wave_vector = [1, 0, 1]
-    delta = 0
-    polarisation = [0, 0, 1]
-    f = 100
+# importing all functions from PoyntingVector module
+from PoyntingVector import electric_field_expression, magnetic_field_expression, poynting_vector_expression, poynting_vector_value, poynting_vector_magnitude
 
-    position = [0, 0, 0]
-    time = 0
 
-    E = electric_field_expression(E_0, wave_vector, delta, f, polarisation)
-    B = magnetic_field_expression(E_0, wave_vector, delta, f, polarisation)
-    S = poynting_vector_expression(E, B)
-    Snum = poynting_vector_value(E, B, position, time)
-    Smag = poynting_vector_magnitude(Snum)
+class PoyntingVectorTest(unittest.TestCase):
 
-    assert Smag - 3978873.5773 < 0.01
+    def test_poynting_vector_value(self): 
+        E = sp.Matrix([1.0, 0.0, 0.0]).T
+        B = sp.Matrix([0.0, 1.0, 0.0]).T
+        position = [0.0, 0.0, 0.0]
+        time = 0.0
+
+        Snum = poynting_vector_value(E, B, position, time) # obtainng the PoyntingVector for trivial case
+        AnswerDirection = sp.Matrix([0.0, 0.0, 1.0]).T # checking if first component is equal to the expected result
+
+        assert Snum[0] == AnswerDirection[0]
+
+    def test_poynting_vector_magnitude(self):
+        S = [3.0, 4.0, 0.0] 
+
+        Smag = poynting_vector_magnitude(S) # obtaining the trivial case PoyntingVector magnitude
+
+        assert Smag == 5 # asserting the value to the expected result
+
+if __name__ == '__main__':
+    unittest.main()
