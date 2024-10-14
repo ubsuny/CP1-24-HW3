eeba0dd3206276b77a1fa0cbe37f4825a8d6b479
abruns123 <aabruns123@gmail.com>
2024-10-14 17:38:21 +0000

    added docstrings and comments to the code as well as a markdown file explaining the physics.

--- electricSphere.py
+++ electricSphere.py
@@ -2,38 +2,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 v=lambda r:4/3*np.pi*r**3
+"""v is the volume of the sphere"""
 
 a=lambda r: r**2*np.pi*4
+"""a is the surface area of a sphere with radius defined by r"""
 
 sv=lambda p,o: p-o
+"""sv is the separation vector between p and o"""
 
 qe=lambda cd,r: v(r)*cd
+"""qe is the charge enclosed by the sphere"""
 
 def getVMag(v):
+    """getVMag returns the magnitude of a 3D vector"""
     return np.sqrt(v[0]**2+v[1]**2+v[2]**2)
 
+    
 def getUVec(v,m):
+    """getUVec returns the unit vector of vector v. m is the magnitude of that vector"""
     return v/m
 
 def inside(r,v):
+    """inside determines whether or not the vector v has greater magnitude r and returns either the magnitude of v or r"""
     if r>=getVMag(v):
-        return getVMag(v)
+        return getVMag(v) 
     else:
         return r
 
 def getFieldMag(cd,r,m):
+    """getFieldMag returns the magnitude of the electric field"""
     eps=8.85*10**-12
     return qe(cd,r)/(eps*a(m))
 
 def zeroCondition(v):
+    """zeroCondition returns a list with a number of elements defined by whether or not the elements of v are zero"""
     return [True for i in v if i==0]
 
 def elecField(o,cd,r,p):
-    m=getVMag(sv(p,o))
-    r=inside(r,sv(p,o))
-    if len(zeroCondition(sv(p,o)))==3:
+    """elecField returns a vector which defines the electric field of a sphere which has its center located at point o, 
+    a charge density cd, and a radius r at point p."""
+
+    m=getVMag(sv(p,o)) #m is the magnitude of the separation vector between p and o
+
+    r=inside(r,sv(p,o)) #if point p is inside the sphere, the radius is reduced to the magnitude of the separation vector
+
+    if len(zeroCondition(sv(p,o)))==3:#if the separation vector is zero, a 3-dimensional zero vector is returned.
         return np.array([0,0,0])
+    
     return getFieldMag(cd,r,m)*getUVec(m,sv(p,o))
+
+def getField(o,cd,r,p):
+    """getField just prints out the result of elecField in an easily readable way."""
+    e=elecField(o,cd,r,p)
+    return "The electric Field is defined by the vector["+str(e[0])+"x,", str(e[1])+"y,",str(e[2])+"z] V/m"
 
 
 
--- electricSphereTester.py
+++ electricSphereTester.py
@@ -6,40 +6,82 @@ from electricSphere import getVMag, getUVec, getFieldMag, elecField, inside, zer
 class TestElectricFieldCalculations(unittest.TestCase):
     
     def test_out(self):
+        """test_out tests whether or not the elecField function can successfully calculate
+        the electric field outside a sphere at the origin"""
+
+        #axis are defined
         x=np.array(np.linspace(5,10,11))
         y=x
         z=x
+
+        #radial distance from origin is defined.
         r=np.array([np.sqrt(3*i**2) for i in x])
+
+        #par contains the parameters for the origin of the sphere, charge density, and radius
         par={"origin":np.array([0,0,0]),"charge density":1,"radius":1}
     
+        #calc1 is the electric field according to the elecField function
         calc1=[getVMag(elecField(par["origin"],par["charge density"], par["radius"],np.array([x[i],y[i],z[i]]))) for i in range(len(x))]
+
+        #calc2 is the electric field according to a theoretical equation
         calc2=[4/3*np.pi*par["radius"]**3*par["charge density"]/(4*np.pi*r[i]*(8.85*10)**-12) for i in range(len(r))]
+
         assert all([True for i in range(len(calc1)) if calc1[i]==calc2[i]])==True
     
     def test_in(self):
+        """test_out tests whether or not the elecField function can successfully calculate
+        the electric field inside a sphere at the origin"""
+
+        #axis are defined
         x=np.array(np.linspace(0,10,11))
         y=x
         z=x
+
+        #radial distance from origin is defined.
         r=np.array([np.sqrt(3*i**2) for i in x])
+
+        #par contains the parameters for the origin of the sphere, charge density, and radius
         par={"origin":np.array([0,0,0]),"charge density":1,"radius":100}
     
+        #calc1 is the electric field according to the elecField function
         calc1=[getVMag(elecField(par["origin"],par["charge density"], par["radius"],np.array([x[i],y[i],z[i]]))) for i in range(len(x))]
+
+        #calc2 is the electric field according to a theoretical equation
         calc2=[4/3*np.pi*r[i]**3*par["charge density"]/(4*np.pi*r[i]*(8.85*10)**-12) for i in range(len(r))]
+
         assert all([True for i in range(len(calc1)) if calc1[i]==calc2[i]])==True
     
     def test_behavior(self):
-        
+        """test_behavior tests whether or not the electric field matches expected behavior
+        While inside the radius, the field should increase in magnitude as the point at which
+        the field is measured approaches the boundary of the sphere and should decrease as
+        the point at which the field is measured moves away from the boundary"""
+
+        #par contains the parameters for the origin of the sphere, charge density, and radius
         par={"origin":np.array([0,0,0]),"charge density":1,"radius":5}
+
+        #axis are defined
         x=np.linspace(0,10,100)
         y=x
         z=x
+
+        #ef will contain a list of electric field vectors
         ef=[]
+
+        #magE will contain a list of the magnitude of the electric field vectors
         magE=[]
+
+        #magV will contain a list of the magnitude of the separation vectors between point p and the origin
         magV=[]
+
+
         for i in range(len(x)):
             ef.append(elecField(par["origin"],par["charge density"],par["radius"],np.array([x[i],y[i],z[i]])))
             magV.append(np.sqrt(x[i]**2+y[i]**2+z[i]**2))
             magE.append(np.sqrt(ef[i][0])**2+ef[i][1]**2+ef[i][2]**2)
+
+        # for each magnitude of the electric field, if point p is inside the radius of the sphere, then the magnitude of the electric field 
+        # must be increasing. If point p is outside the radius of the sphere, then the magnitude of the electric field must be decreasing
         for i in range(1,len(magE)):
         
             if magV[i]<=par["radius"]:
@@ -49,18 +91,25 @@ class TestElectricFieldCalculations(unittest.TestCase):
                 assert magE[i]<magE[i-1]
     
     def test_notInner(self):
+        """test_notInner tests whether or not the inside function can accurately determine if point p is outside the radius
+        of the sphere"""
+
         o=np.array([0,0,0])
         v=np.array([10,10,10])
         r=5
         assert inside(r,(v-o))==r
     
     def test_inner(self):
+        """test_Inner tests whether or not the inside function can accurately determine if point p is within the radius
+        of the sphere"""
+
         o=np.array([0,0,0])
         v=np.array([10,10,10])
         r=100
         assert inside(r,(v-o))==getVMag(v)
 
     def test_zero(self):
+        """test_zero tests whether or not the elecField function accounts for the separation vector being zero"""
         o=np.array([0,0,0])
         v=o
         r=1
@@ -69,6 +118,8 @@ class TestElectricFieldCalculations(unittest.TestCase):
             assert elecField(o,cd,r,v)[i]==0
     
     def test_zeroCondition(self):
+        "test_zeroCondition tests whether or no zeroCondition is functioning correctly for possible vectors."
+
         v=([0,0,1])
         assert len(zeroCondition(v))==2
         v=([0,1,1])
