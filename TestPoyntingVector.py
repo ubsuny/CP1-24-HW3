6f838c0ac575ab1f61eafc656df554deb33f62b4
ojha-aditya <adityaojha.science@gmail.com>
2024-10-16 13:53:50 +0000

    Removed Binary files

--- TestPoyntingVector.py
+++ TestPoyntingVector.py
@@ -1,23 +1,28 @@
-import PoyntingVector
 import numpy as np
 import sympy as sp
 import math
-import pytest
+import unittest
+from PoyntingVector import electric_field_expression, magnetic_field_expression, poynting_vector_expression, poynting_vector_value, poynting_vector_magnitude
 
-def Test_PoyntingVector():
-    E_0 = 5
-    wave_vector = [1, 0, 1]
-    delta = 0
-    polarisation = [0, 0, 1]
-    f = 100
+class PoyntingVectorTest(unittest.TestCase):
 
-    position = [0, 0, 0]
-    time = 0
+    def test_poynting_vector_value(self):
+        E = sp.Matrix([1.0, 0.0, 0.0]).T
+        B = sp.Matrix([0.0, 1.0, 0.0]).T
+        position = [0.0, 0.0, 0.0]
+        time = 0.0
 
-    E = electric_field_expression(E_0, wave_vector, delta, f, polarisation)
-    B = magnetic_field_expression(E_0, wave_vector, delta, f, polarisation)
-    S = poynting_vector_expression(E, B)
-    Snum = poynting_vector_value(E, B, position, time)
-    Smag = poynting_vector_magnitude(Snum)
+        Snum = poynting_vector_value(E, B, position, time)
+        AnswerDirection = sp.Matrix([0.0, 0.0, 1.0]).T
 
-    assert Smag - 3978873.5773 < 0.01
+        assert Snum[0] == AnswerDirection[0]
+
+    def test_poynting_vector_magnitude(self):
+        S = [3.0, 4.0, 0.0] 
+
+        Smag = poynting_vector_magnitude(S)
+
+        assert Smag == 5
+
+if __name__ == '__main__':
+    unittest.main()
