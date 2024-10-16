3429cad6a40839932803da7b0d72e86b5e88619e
abruns123 <aabruns123@gmail.com>
2024-10-16 15:34:35 +0000

    I fixed the primary pylint errors by adjusting the lambda functions and added a doc string for the main function and adjusted the docstring describing get_field

--- electricSphere.py
+++ electricSphere.py
@@ -5,18 +5,28 @@ by a sphere with constant charge density placed at another
 point in space.
 """
 import numpy as np
+def get_volume():
+    """returns a function defining the volume of the sphere"""
+    return lambda r:4/3*np.pi*r**3
 
-v=lambda r:4/3*np.pi*r**3
-"""v is the volume of the sphere"""
+def get_area():
+    """returns a function defining the surface area of a sphere"""
+    return lambda r: r**2*np.pi*4
 
-a=lambda r: r**2*np.pi*4
-"""a is the surface area of a sphere with radius defined by r"""
+def get_sep():
+    """
+    returns a function defining the separation vector
+    between the center of the sphere and the point where
+    electric field is measured
+    """
+    return lambda p,o: p-o
 
-sv=lambda p,o: p-o
-"""sv is the separation vector between p and o"""
+def get_qe():
+    """
+    Returns a function defining the charge enclosed by the sphere
+    """
+    return lambda cd,r: get_volume()(r)*cd
 
-qe=lambda cd,r: v(r)*cd
-"""qe is the charge enclosed by the sphere"""
 
 def get_vmag(v1):
     """getVMag returns the magnitude of a 3D vector"""
@@ -39,7 +49,7 @@ def inside(r,v1):
 def get_field_mag(cd,r,m):
     """getFieldMag returns the magnitude of the electric field"""
     eps=8.85*10**-12
-    return qe(cd,r)/(eps*a(m))
+    return get_qe()(cd,r)/(eps*get_area()(m))
 
 def zero_condition(v1):
     """zeroCondition returns a list with a number of elements 
@@ -56,20 +66,23 @@ def elec_field(o,cd,r,p):
     radius r at point p.
     """
 
-    m=get_vmag(sv(p,o)) #m is the magnitude of the separation vector between p and o
-    r=inside(r,sv(p,o))
+    m=get_vmag(get_sep()(p,o)) #m is the magnitude of the separation vector between p and o
+    r=inside(r,get_sep()(p,o))
     #if point p is inside the sphere, the radius is
     # reduced to the magnitude of the separation vector
 
-    if len(zero_condition(sv(p,o)))==3:
+    if len(zero_condition(get_sep()(p,o)))==3:
     #if the separation vector is zero, a 3-dimensional zero vector is returned.
 
         return np.array([0,0,0])
 
-    return get_field_mag(cd,r,m)*get_uvec(sv(p,o),m)
+    return get_field_mag(cd,r,m)*get_uvec(get_sep()(p,o),m)
 
 def get_field(o,cd,r,p):
-    """getField just prints out the result of elecField in an easily readable way."""
+    """
+    getField just puts the electric field into a string datatype
+    such that it may be easily read by the user.
+    """
     e=elec_field(o,cd,r,p)
     return (
         "The electric Field is defined by the vector\
@@ -77,4 +90,14 @@ def get_field(o,cd,r,p):
     )
 
 if __name__=="__main__":
-    print(get_field(np.array([0,0,0]), 1, .5, np.array([.8,.9,.3])))
+    """
+    This main loop allows the user to make a
+    sample calculation of an electric field
+    of a sphere located at origin with charge density
+    qd and radius rad at a point p in the x,y,z space.
+    """
+    origin=np.array([0,0,0])
+    QD=1
+    RAD=5
+    po=np.array([.8,.9,.3])
+    print(get_field(origin, QD, RAD, po))
