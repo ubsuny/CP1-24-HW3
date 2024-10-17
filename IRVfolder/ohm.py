e30c5c8a2d183207653d460afa525a13f8fcc896
zbpetersbuf <zbpeters@buffalo.edu>
2024-10-17 01:09:01 +0000

    fixedsomeissues

--- IRVfolder/ohm.py
+++ IRVfolder/ohm.py
@@ -1,22 +1,25 @@
 import numpy as np
-def ohmscur(r, v):
-    div = lambda x, y: x/y
+def ohmscur(r,v):
+    """This function finds current using ohms law"""
+    div = lambda x,y: x/y
     if int(r)==0:
         raise ZeroDivisionError("Your resistance cannot be zero when finding current, try again")
     else:
         print ("Your current is",div(v,r))
         return div(v,r)
-def ohmsres(i, v):
-    div = lambda x, y: x/y
+def ohmsres(i,v):
+    """This function finds resistance using ohms law"""
+    div = lambda x,y: x/y
     if int(i)==0:
         raise ZeroDivisionError("Curent cannot be zero when finding resistance, try again")
     else:
         print ("Your resistance is",div(v,i))
         return div(v,i)
-def ohmsvol(i, r):
-    mult = lambda x, y: x*y
+def ohvol(i,r):
+    """This function finds voltage using ohms law"""
+    mult = lambda x,y: x*y
     print ("Your voltage is",mult(i,r))
     return mult(i,r)
-
-def batch_ohmscur(x1List,x2List,func):
-    return [IRV(x1,x2) for x1,x2 in zip(x1List,x2List)]
+def batch_ohm(x1list,x2list,func):
+    """This function allows the use of lists from the previos functions and out puts a list of awnsers"""
+    return [func(x1,x2) for x1,x2 in zip(x1list,x2list)]
--- IRVfolder/test_ohm.py
+++ IRVfolder/test_ohm.py
@@ -1,15 +1,16 @@
 import numpy as np
-
-from IRVfolder.ohm import ohmscur, ohmsres, ohmsvol
-
+from ohm import ohmscur, ohmsres, ohvol, batch_ohm
 def ohmscur():
-    """ This test the ohmscur function """
+    """This tests the ohmscur function"""
     assert ohmscur(2,3)==1.5
-
 def ohmsres():
-    """ This test the ohmsres function """
+    """This tests the ohmsres function"""
     assert ohmsres(2,1)==2.0
-
-def ohmsvol():
-    """ This test the ohmsvol function """
-    assert ohmsvol(3,1)==3.0
+def ohvol():
+    """This tests the ohmsvol function"""
+    assert ohvol(3,1)==3.0
+curlis=[1,2,3];reslis=[2,3,4];volist=[2.0,6.0,12.0]
+from ohm import ohvol
+def batch_ohm():
+    """This tests the batch_ohmsl function curlist is the list of currents, reslis is the list of resistances, volist is the expected voltages"""
+    assert batch_ohm(curlis,reslis,ohvol)==volist
