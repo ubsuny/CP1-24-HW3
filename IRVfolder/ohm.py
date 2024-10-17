d44f0053165e1fbd01504d9f1f07019968e3bde0
zbpetersbuf <zbpeters@buffalo.edu>
2024-10-17 01:48:54 +0000

    deleated the IRV file and finalized the ohm and test_ohm

--- IRVfolder/ohm.py
+++ IRVfolder/ohm.py
@@ -1,25 +1,32 @@
-import numpy as np
-def ohmscur(r,v):
+"""
+ohm.py
+
+This module contains functions for calculating ohms law for three diffrent case,
+when you have resistance and voltage and you need to find current use ohmscur 
+when you have current and voltage and you need to find resistance use ohmsres
+and for the final case of finding voltage use ohval
+and batch_ohm takes 2 lists of values and the function you want to use and returns
+a list of values calculated by the function entered
+"""
+def ohmscur(r, v):
     """This function finds current using ohms law"""
-    div = lambda x,y: x/y
+    div = lambda x, y: x/y
     if int(r)==0:
         raise ZeroDivisionError("Your resistance cannot be zero when finding current, try again")
-    else:
-        print ("Your current is",div(v,r))
-        return div(v,r)
-def ohmsres(i,v):
+    print ("Your current is",div(v, r))
+    return div(v, r)
+def ohmsres(i, v):
     """This function finds resistance using ohms law"""
-    div = lambda x,y: x/y
+    div = lambda x, y: x/y
     if int(i)==0:
         raise ZeroDivisionError("Curent cannot be zero when finding resistance, try again")
-    else:
-        print ("Your resistance is",div(v,i))
-        return div(v,i)
-def ohvol(i,r):
+    print ("Your resistance is",div(v, i))
+    return div(v, i)
+def ohvol(i, r):
     """This function finds voltage using ohms law"""
-    mult = lambda x,y: x*y
-    print ("Your voltage is",mult(i,r))
-    return mult(i,r)
-def batch_ohm(x1list,x2list,func):
-    """This function allows the use of lists from the previos functions and out puts a list of awnsers"""
-    return [func(x1,x2) for x1,x2 in zip(x1list,x2list)]
+    mult = lambda x, y: x*y
+    print ("Your voltage is",mult(i, r))
+    return mult(i, r)
+def batch_ohm(x1list, x2list, func):
+    """This function allows the use of lists and outputs its own"""
+    return [func(x1, x2) for x1, x2 in zip(x1list, x2list)]
--- IRVfolder/test_ohm.py
+++ IRVfolder/test_ohm.py
@@ -1,16 +1,23 @@
-import numpy as np
+"""
+test_ohm.py
+
+This is the test module for the functions defined in ohm.py
+"""
 from ohm import ohmscur, ohmsres, ohvol, batch_ohm
-def ohmscur():
+def test_ohmscur():
     """This tests the ohmscur function"""
-    assert ohmscur(2,3)==1.5
-def ohmsres():
+    assert ohmscur(2, 3)==1.5
+def test_ohmsres():
     """This tests the ohmsres function"""
-    assert ohmsres(2,1)==2.0
-def ohvol():
+    assert ohmsres(2, 1)==2.0
+def test_ohvol():
     """This tests the ohmsvol function"""
-    assert ohvol(3,1)==3.0
-curlis=[1,2,3];reslis=[2,3,4];volist=[2.0,6.0,12.0]
-from ohm import ohvol
-def batch_ohm():
-    """This tests the batch_ohmsl function curlist is the list of currents, reslis is the list of resistances, volist is the expected voltages"""
-    assert batch_ohm(curlis,reslis,ohvol)==volist
+    assert ohvol(3, 1)==3.0
+curlis=[1, 2, 3]
+reslis=[2, 3, 4]
+"""curlist is the list of currents, reslis is the list of resistances"""
+volist=[2.0, 6.0, 12.0]
+"""volist is the expected voltages"""
+def test_batch_ohm():
+    """This tests the batch_ohmsl function"""
+    assert batch_ohm(curlis, reslis, ohvol)==volist
