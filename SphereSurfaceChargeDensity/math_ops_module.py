fd68e935624e9d9cc1ee9c48844dac2daa2290a9
HeshamElsaman <88336539+HeshamElsaman@users.noreply.github.com>
2024-10-17 02:59:42 +0000

    Followed the linting instructions as much as possible

--- SphereSurfaceChargeDensity/math_ops_module.py
+++ SphereSurfaceChargeDensity/math_ops_module.py
@@ -1,21 +1,27 @@
-""" This Module is to define the general mathematical operations to be used by the other modules """
+"""
+This Module is to define the general mathematical operations to be used by the other modules
+"""
 
 def div(numerator, denominator):
     """
     Returns the result of dividing two numbers
+    
     Parameters:
     numerator, denominator: input numbers
+    
     Returns:
-    number: The result of dividing the first number by the second number utilizing the lambda expression
+    number: The result of dividing the first number by the second
     """
     return numerator / denominator
 
 def mult(x, y):
     """
     Returns the result of multiplying two numbers
+    
     Parameters:
     x, y: input numbers
+    
     Returns:
-    number: The result of multiplying the two numbers utilizing the lambda expression
+    number: The result of multiplying the two numbers
     """
     return x * y
--- SphereSurfaceChargeDensity/physics_module.py
+++ SphereSurfaceChargeDensity/physics_module.py
@@ -1,6 +1,7 @@
 """
 This Module is to define the main desired funtion:
-    A function to calculate the surface charge density of a conducting spherical shell held at constant potential.
+    A function to calculate the surface charge density
+    of a conducting spherical shell held at constant potential.
 
 The specifications needed for the shell:
     The radius (R): a float number.
@@ -11,14 +12,17 @@ However, it will be flexible to convert to other systems by manually choosing th
 A constant value of (epsilon_0) is thus set by default to the units of the metric system.
 """
 
-from MathOpsModule import *
+from math_ops_module import div, mult
 
-def sigma(R, V, epsilon_0 = 8.854*(10**(-12))):
+def sigma(radius, potential, epsilon_0 = 8.854*(10**(-12))):
     """
-    Returns the surface charge density of a conducting spherical shell of radius (R) held at constant potential (V).
+    Returns the surface charge density of a conducting spherical
+    shell of radius (R) held at constant potential (V).
+
     Parameters:
     R, V, epsilon_0: input numbers (radius, potential, electric permitivity of free space)
+    
     Returns:
     number: the surface charge density
     """
-    return mult(epsilon_0, div(V, R))
+    return mult(epsilon_0, div(potential, radius))
